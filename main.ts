af66c3d016c8614487fd13a38e2e54ee8909ad3e
Tanabe <tanabe2852@yahoo.co.jp>
2022-05-18 09:17:07 +0900

    制御基板に接続

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 function _74HC4094 (value: number) {
     pins.digitalWritePin(DigitalPin.P16, 0)
     i = 1
-    for (let index = 0; index < 8; index++) {
+    for (let index = 0; index < 16; index++) {
         tmp = Math.trunc(value / i)
         pins.digitalWritePin(DigitalPin.P14, tmp % 2)
         clk()
@@ -21,16 +21,8 @@ basic.clearScreen()
 led.enable(false)
 pins.setPull(DigitalPin.P6, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P7, PinPullMode.PullNone)
+pins.setPull(DigitalPin.P9, PinPullMode.PullNone)
 basic.forever(function () {
-    while (true) {
-        if (pins.digitalReadPin(DigitalPin.P6) == 0) {
-            pins.digitalWritePin(DigitalPin.P0, 0)
-        } else {
-            pins.digitalWritePin(DigitalPin.P0, 1)
-        }
-    }
-    _74HC4094(15)
-    basic.pause(1000)
-    _74HC4094(240)
+    _74HC4094(1000)
     basic.pause(1000)
 })
