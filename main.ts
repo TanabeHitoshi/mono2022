12014ba003739e54716dd6ff2857ae9bc8346851
Tanabe <tanabe2852@yahoo.co.jp>
2022-06-04 20:17:17 +0900

    各問題を関数に

--- main.ts
+++ main.ts
@@ -1,7 +1,68 @@
-custom.initialize()
-let patern = 0
-basic.forever(function () {
-    if (custom.入力(io.digital01) == custom.出力(out.ON)) {
+function 問題6 () {
+    while (true) {
     	
+    }
+}
+function 問題1 () {
+    while (true) {
+    	
+    }
+}
+function 問題3 () {
+    while (true) {
+    	
+    }
+}
+function 問題7 () {
+    while (true) {
+    	
+    }
+}
+function 問題4 () {
+    while (true) {
+    	
+    }
+}
+function 問題8 () {
+    while (true) {
+    	
+    }
+}
+function 問題2 () {
+    while (true) {
+    	
+    }
+}
+function 問題5 () {
+    while (true) {
+    	
+    }
+}
+custom.initialize()
+let 問題 = 1
+basic.forever(function () {
+    if (問題 == 1) {
+        問題1()
+    }
+    if (問題 == 2) {
+        問題2()
+    }
+    if (問題 == 3) {
+        問題3()
+    }
+    if (問題 == 4) {
+        問題4()
+    }
+    if (問題 == 5) {
+        問題5()
+    }
+    if (問題 == 6) {
+        問題6()
+    }
+    if (問題 == 7) {
+        問題7()
+    }
+    if (問題 == 8) {
+        問題8()
     }
 })
