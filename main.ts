bb5888d9e2623458eb3cfa90e9cbff348828e199
Tanabe <tanabe2852@yahoo.co.jp>
2022-07-05 17:43:59 +0900

    num配列の作成

--- main.ts
+++ main.ts
@@ -18,6 +18,19 @@ let デジタル０１ = 1
 let デジタル０２ = 2
 let アナログ０３ = 3
 let アナログ０４ = 4
+let num = [
+252,
+96,
+218,
+242,
+102,
+182,
+190,
+228,
+254,
+246,
+0
+]
 basic.forever(function () {
     custom.セグメントＬＥＤ(LED_view.Hi, LED_view.Low)
 })
