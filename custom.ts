10681b27da95625035fba91137cbdd708c858d6b
Tanabe <tanabe2852@yahoo.co.jp>
2022-07-05 17:39:32 +0900

    クロック関数を１つなくした

--- custom.ts
+++ custom.ts
@@ -351,7 +351,11 @@ namespace custom {
         for (let index = 0; index < 16; index++) {
             let tmp = Math.trunc(value / i)
             pins.digitalWritePin(DigitalPin.P14, tmp % 2)
-            clk()
+            //クロック発生
+            pins.digitalWritePin(DigitalPin.P13, 0)
+            basic.pause(1)
+            pins.digitalWritePin(DigitalPin.P13, 1)
+            basic.pause(1)
             i = i * 2
         }
         pins.digitalWritePin(DigitalPin.P16, 1)
