12d3ca56bae90ea7d08f971c474031b2f4c15d4a
Tanabe <tanabe2852@yahoo.co.jp>
2022-07-24 13:17:11 +0900

    音のプログラム

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
 custom.config_ini()
-let デジタル０１ = 1
-let デジタル０２ = 2
+let 上下 = 3
+let 左右 = 4
+let デジタル０１ = 6
+let デジタル０２ = 7
+let 本体タクト = 9
+let モーター_1 = 8
+let モーター_2 = 12
 let num = [
 252,
 96,
@@ -15,16 +20,8 @@ let num = [
 1
 ]
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P6) == 1) {
-        pins.digitalWritePin(DigitalPin.P12, 0)
-        pins.analogWritePin(AnalogPin.P8, 1023)
-    } else {
-        if (pins.digitalReadPin(DigitalPin.P9) == 0) {
-            pins.digitalWritePin(DigitalPin.P8, 0)
-            pins.analogWritePin(AnalogPin.P12, 1023)
-        } else {
-            pins.digitalWritePin(DigitalPin.P8, 0)
-            pins.digitalWritePin(DigitalPin.P12, 0)
-        }
-    }
+    music.ringTone(262)
+    basic.pause(2000)
+    music.stopAllSounds()
+    basic.pause(2000)
 })
